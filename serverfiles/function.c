369c4b66cd044598c7f341fd5d852aca581e5277
yamin <Yamin.Ahmed@outlook.de>
2024-08-06 16:52:20 +0200

    we will move later on

--- serverfiles/function.c
+++ serverfiles/function.c
@@ -1,7 +1,44 @@
 // static buffers for frontend
+#include <string.h>
 
 char HTML_BUFFER[1024];
 char CSS_BUFFER[1024];
 char JS_BUFFER[1024];
 
 
+char HTTP_RESPONSE[2048];
+
+
+void genHTML_HEADER()
+{
+
+}
+
+void genCSS_HEADER()
+{
+
+}
+
+void genJS_HEADER()
+{
+
+}
+
+
+void CONNECT_HTTP_RESPONSE_BUFFER(char* BUFFER, char type)
+{
+    memset(HTTP_RESPONSE,0,sizeof(HTTP_RESPONSE));
+    HTTP_RESPONSE[2047] = '\0';
+    
+    switch(type)
+    {
+        case 'h': genHTML_HEADER();
+        break;
+
+        case 'c': genCSS_HEADER();
+        break;
+
+        case 'j': genJS_HEADER();
+        break;
+    }
+}
